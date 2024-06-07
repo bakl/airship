fccd0c89061f207b61ec62026e284989e8cc9955
bakl <bakl813@yandex.ru>
2024-06-07 22:39:47 +0300

    Release v1.2.25

--- src/deploy.py
+++ src/deploy.py
@@ -11,7 +11,7 @@ import json
 import http.client
 from datetime import datetime
 
-version = "v1.2.24"
+version = "v1.2.25"
 
 def usage():
     print("AirShip [%s] usage: deploy.py [server name] {commands} {options}" % version)
